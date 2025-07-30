c08647f10fe66df7d93e5ccdd7ae5bc91389b04f
Stephen Barker <StephenBarker@users.noreply.github.com>
2025-07-30 18:36:12 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,1 @@
-# deploy.sh
-#!/bin/bash
 
-# Build the site
-hugo
-
-# Go to public directory and push to gh-pages branch
-cd public
-git init
-git remote add origin https://github.com/StephenBarker/stephenbarker.git
-git checkout -b gh-pages
-git add .
-git commit -m "Deploy"
-git push -f origin gh-pages
-
-cd ..
